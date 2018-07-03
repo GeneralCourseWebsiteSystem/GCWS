b86e224c19ea0c38f3b6627c5738da34a52e01b1
Administrator <Administrator@PC-20161015CSGS>
2018-07-03 20:29:15 +0800

    地方的方法、

--- GCWS/src/com/cqut/userManagement/dao/UserManagementDao.java
+++ GCWS/src/com/cqut/userManagement/dao/UserManagementDao.java
@@ -29,7 +29,19 @@ public interface UserManagementDao {
 	/**
 	 * 获取所有的用户角色
 	 */
-	public ArrayList<RoleUserLink> getAllRole();
+	public ArrayList<RoleUserLink> getAllRoleA();
+	
+	/**
+	 * 获取所有的用户角色分页展示
+	 */
+	public ArrayList<RoleUserLink> getAllRole(Integer index, Integer limit, String str);
+	
+	/**
+	 * 获取模糊搜索的数量
+	 * @param str
+	 * @return
+	 */
+	public Integer getAllRoleNum(String str) ;
 	
 	/**
 	 * 获取所有用户及其角色
@@ -42,6 +54,18 @@ public interface UserManagementDao {
 	 * 进行角色分配(传用户id和分配的角色id)
 	 */
 	public boolean role_allocaion(int userId,int roleId);
+		
+	/**
+	 * 获得用户信息
+	 * @param myaccount
+	 * @return
+	 */
+	public User getId(String myaccount);
 	
-	
+	/**
+	 * 获得用户身份
+	 * @param id
+	 * @return
+	 */
+	public RoleAll getRoleId(int id);
 }
